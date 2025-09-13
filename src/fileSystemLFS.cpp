7ec21adcfdf14c16e905a5d4761c5dae2209b533
Corvette Rox <development@thzero.com>
2025-09-13 08:59:52 -0500

    removed need for constants

--- src/fileSystemLFS.cpp
+++ src/fileSystemLFS.cpp
@@ -2,7 +2,6 @@
 #include <ArduinoJson.h>
 #include <LittleFS.h>
 
-#include "constants.h"
 #include <debug.h>
 #include "fileSystemLFS.h"
 #include <utilities.h>
@@ -30,10 +29,8 @@ bool fileSystemLFS::setup() {
     // format if begin fails
     Serial.println(F("\tLittleFS trying to format..."));
     if (!LittleFS.begin(true)) {
-#if defined(DEBUG) || defined(DEBUG_LOGGER)
       Serial.println(F("\tLittleFS format failed"));
       Serial.println(F("\tFormatting not possible"));
-#endif
 
       Serial.println(F("...file system setup failed."));
       return false;
