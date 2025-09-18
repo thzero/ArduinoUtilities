23eefce4e65926556091742311f5b36794caeda1
Corvette Rox <development@thzero.com>
2025-09-18 16:04:27 -0500

    button handler for a specific button

---
+++ src/buttonHandlerBase.h
@@ -0,0 +1,20 @@
+#ifndef _BUTTON_BASE_H
+#define _BUTTON_BASE_H
+
+#include <Arduino.h>
+#include <Button2.h>
+
+class buttonHandlerBase {
+  public:
+    void deinit();
+    void loop();
+    void setup(char name, int pin);
+    virtual void click(Button2 button);
+    virtual void longClick(Button2 button);
+    char name();
+  private:
+    Button2 _btn;
+    char _name;
+};
+
+#endif
